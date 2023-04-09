73c4a625a7e9730cbf444ec812401d29004aba52
jewelrana3 <juyelrana7752@gmail.com>
2023-04-09 12:28:38 +0600

    navber

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
-      <h3>App file</h3>
+      <Header></Header>
       <Outlet></Outlet>
+      <h3>App file</h3>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { BoltIcon } from '@heroicons/react/24/solid'
+import { Link, NavLink } from 'react-router-dom';
+import './Header'
 
 const Header = () => {
     return (
-        <div>
-            header
+        <div className='flex justify-between mx-6'>
+            <Link to='/' className='inline-flex items-center'>
+                <BoltIcon className='h-6 w-6 text-blue-300 mr-3' />
+                <span className='text-xl font-bold text-gray-800'>Next Page</span>
+            </Link>
+           {/* nav link */}
+
+           <ul className='items-center hidden lg:flex space-x-8'>
+                <li>
+                    <NavLink to='/' className={({isActive})=> (isActive ? 'text-blue-400' : '')}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='books' className={({isActive})=> (isActive ? 'text-blue-400' : '')}>
+                        Books
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='about' className={({isActive})=> (isActive ? 'text-blue-400' : '')}>
+                        About
+                    </NavLink>
+                </li>
+           </ul>
         </div>
     );
 };
